86b920efad7f3f333dc4f0ce53111ee696bc41f8
laviedegeorge <laviedegeorge1292@gmail.com>
2019-07-12 15:36:57 +0100

    deleted every other folder

--- basic-hide-reveal-transition/basichide.js
+++
@@ -1,7 +0,0 @@
-let btn = document.getElementById("btn");
-btn.addEventListener("click", 
-    reveal = () => {
-        let revealDiv = document.getElementById("revealDiv");
-        revealDiv.classList.toggle('reveal');
-        }
-);
